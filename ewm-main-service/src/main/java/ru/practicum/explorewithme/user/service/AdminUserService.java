d151184032fb38e7d51f957c470f98e26941e866
Ekaterina-14-06 <98764172+Ekaterina-14-06@users.noreply.github.com>
2022-10-30 15:03:33 +0300

    Update AdminUserService.java

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/user/service/AdminUserService.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/user/service/AdminUserService.java
@@ -1,11 +1,10 @@
 package ru.practicum.explorewithme.user.service;
 
 import ru.practicum.explorewithme.user.dto.UserDto;
-
 import java.util.List;
 
 public interface AdminUserService {
-    List<UserDto> getUsers(List<Long> users, int from, int size);
     UserDto postUser(UserDto userDto);
     void deleteUser(long userId);
+    List<UserDto> getUsers(List<Long> users, int from, int size);
 }
